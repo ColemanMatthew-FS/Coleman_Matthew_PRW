6bd0732fe66c8c3f317d51fd71d1ec345819b0b4
Matthew Coleman <MRColeman1@student.fullsail.edu>
2022-02-28 16:59:15 -0500

    troubleshooting package-lock

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 
 class App extends Component() {
   render() {
+
     return (
       <div className="App">
-        <p>Testing</p>
+        <p>testing</p>
+        <div>some stuff</div>
       </div>
     );
   }
 }
-
 export default App;
+/*function App() {
+  return (
+      <div className="App">
+        <p>testing</p>
+      </div>
+    );
+}
+
+export default App;*/
